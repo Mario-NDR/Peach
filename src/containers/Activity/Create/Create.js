1eff8287a728506a1f45791170d1b72754b80add
cuiqs <cuiqs@knownsec.com>
2019-08-11 14:31:39 +0800

    feat: 活动创建界面 done

--- src/containers/Activity/Create/Create.js
+++ src/containers/Activity/Create/Create.js
@@ -2,10 +2,13 @@
  * @summary 活动创建
  */
 import React from 'react'
+import { Radio, Button } from 'antd'
 
 import { IntlComponent } from 'Components/Common'
 import Bread from 'Components/Bread'
 import Step from 'Components/Step'
+import ContentBox from 'Components/ContentBox'
+import Subheader from 'Components/Subheader'
 
 import style from './style.scss'
 
@@ -13,7 +16,16 @@ class Create extends IntlComponent {
 
   constructor(props) {
     super(props)
-    this.state = {}
+    this.state = {
+      value: 1,
+    }
+  }
+
+  onTypeChange = e => {
+    console.log('radio checked', e.target.value)
+    this.setState({
+      value: e.target.value,
+    })
   }
 
   render() {
@@ -26,7 +38,8 @@ class Create extends IntlComponent {
             { content: '活动类型选择' },
           ]}
         />
-        <div>
+        <ContentBox>
+          <Subheader>活动创建</Subheader>
           <Step
             items={[
               { title: '活动类型选择' },
@@ -35,7 +48,20 @@ class Create extends IntlComponent {
               { title: '活动管理/审批' },
             ]}
           />
-        </div>
+          <div className="subtitle" style={{ marginTop: 32 }}>活动类型</div>
+          <div>
+            <span className={style.label}>选择活动类型：</span>
+            <Radio.Group onChange={this.onTypeChange} value={this.state.value}>
+              <Radio value={1}>报名活动</Radio>
+              <Radio value={2}>投票活动</Radio>
+              <Radio value={3}>竞猜活动</Radio>
+              <Radio value={4}>抽奖活动</Radio>
+            </Radio.Group>
+          </div>
+          <div className="flexEnd">
+            <Button type="primary" size="large">下一步：活动内容配置</Button>
+          </div>
+        </ContentBox>
       </div>
     )
   }
