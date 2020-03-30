fe7e881bffb35dfb2b62a03ad17fde60181d8f67
jinghh <you@example.com>
2020-03-30 18:03:29 +0800

    fix: reducer map

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -4,7 +4,7 @@ import { routerReducer } from 'react-router-redux'
 import mapReducer from 'Containers/Home/reducer'
 import loginReducer from 'Containers/Login/reducer'
 import attackTrafficReducer from 'Containers/Activity/reducer'
-import rulesReducer from 'Containers/Activity/PrizeLibrary/reducer'
+import rulesReducer from 'Containers/Activity/Rules/reducer'
 
 const createReducer = (asyncReducers) => {
   return combineReducers({
